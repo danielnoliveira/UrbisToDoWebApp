903c617d7cc0fbb43992bfcb29d1d201bd75f98c
Daniel Nogueira de Oliveira <daniel.n.oliveira@outlook.com>
2021-04-02 16:28:03 -0300

    User: adicionado celebrate a rota login; ToDo: adicionado celebrate as rotas item/create e item/update

--- backend/src/Routes/ToDo.js
+++ backend/src/Routes/ToDo.js
@@ -1,7 +1,10 @@
 const ToDoController = require('../Controllers/ToDo');
+const {celebrate} = require('celebrate');
+const {createItem,updateItem} = require('./celebrates');
+
 module.exports = (router) => {
     router.route('/list/items').get(ToDoController.getList);
-    router.route('/list/item/create').post(ToDoController.createItem);
-    router.route('/list/item/update').put(ToDoController.updateItem);
+    router.route('/list/item/create').post(celebrate(createItem),ToDoController.createItem);
+    router.route('/list/item/update').put(celebrate(updateItem),ToDoController.updateItem);
     router.route('/list/item/delete').delete(ToDoController.deleteItem);
 }
--- backend/src/Routes/User.js
+++ backend/src/Routes/User.js
@@ -1,5 +1,6 @@
 const UserController = require('./../Controllers/User');
-
+const {celebrate} = require('celebrate');
+const validaters = require('./celebrates');
 module.exports = (router) => {
-    router.route('/user/login').post(UserController.login);
+    router.route('/user/login').post(celebrate(validaters.login),UserController.login);
 }
