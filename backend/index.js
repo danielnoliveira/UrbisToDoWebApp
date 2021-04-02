e8dd2a9b9730e8dcb37a589306cfa11bc7aebd3c
Daniel Nogueira de Oliveira <daniel.n.oliveira@outlook.com>
2021-04-02 15:15:34 -0300

    Servidor configurado

---
+++ backend/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+
+const routes = require('./src/Routes/index');
+const router = express.Router();
+
+app.get('/',(req,res) => {
+    res.send("<h1>Bem vindo ao Urbis ToDo WebApp</h1>");
+});
+
+app.use('/',routes(router));
+
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Servidor iniciado!!!");
+});
