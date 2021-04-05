622da97e1645b59200614371ed01220018379105
Daniel Nogueira de Oliveira <daniel.n.oliveira@outlook.com>
2021-04-05 11:42:10 -0300

    celebrates validators created

--- backend/src/Routes/celebrates.js
+++ backend/src/Routes/celebrates.js
@@ -3,13 +3,18 @@ module.exports = {
     createItem:{
         [Segments.BODY]: Joi.object().keys({
             name:Joi.string().required(),
-            date:Joi.date().required(),
         }),
     },
     updateItem: {
-        [Segments.QUERY]: Joi.object().keys({
+        [Segments.BODY]: Joi.object().keys({
+            id_task: Joi.number().integer().required(),
             name: Joi.string().optional(),
-            date: Joi.date().optional(),
+            concluded: Joi.boolean().optional(),
+        }),
+    },
+    deleteItem:{
+        [Segments.BODY]: Joi.object().keys({
+            id_task: Joi.number().integer().required(),
         }),
     },
     login: {
